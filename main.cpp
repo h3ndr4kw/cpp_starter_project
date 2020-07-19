6d44db4811c1d32d23335ea7685cec1cc0ca8e13
abualyya <abuallya@github.com>
2020-07-19 07:15:20 +0700

    add run time

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 #include <functional>
+#include <chrono>
 
 int main()
 {
+  auto begin = std::chrono::high_resolution_clock::now();
   [out = std::ref(std::cout << "Hello ")](){ out.get() << "World\n";}();
+
+  for (int i = 0; i < 1e9; i++)
+  {
+    /* code */
+    std::cout << i << std::endl;
+  }
+  
+
+
+  auto end = std::chrono::high_resolution_clock::now();
+
+  double duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end-begin).count();
+
+  std::cout << "Time Elapsed : " << duration << " nanoseconds" << std::endl;
+
 }
